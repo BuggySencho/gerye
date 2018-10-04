071bbb8075a764d9ebae003a9167401bb52cfa1a
ihartog <26035@ma-web.nl>
2018-10-04 09:17:35 +0100

    Initial commit

---
+++ src/conditionals.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class conditionals {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("wat is je leeftijd:");
+        int age = scan.nextInt();
+
+        if (age >= 18) {
+            System.out.println("Je bent oud genoeg om alcohol te kopen");
+        } else {
+
+            System.out.println("Je mag nog geen alcohol kopen");
+        }
+    }}
+
+
+
+
+
+
+
+
+  //      Boolean teacherTalks = true;
+  //
+  //      if (teacherTalks != true) {
+  //
+  //          System.out.println("Ok, teacher talks");
+  //      } else {
+  //
+  //          System.out.println("teacher does not talk");
+  //
+  //      }
+
+
